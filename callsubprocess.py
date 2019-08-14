aa2cf90388ac5565c6d8d61fb0e4a2f4d71c3f1a
tentakuru <53518872+tentakuru@users.noreply.github.com>
2019-08-14 19:22:12 +0200

    Correctly send arguments
    Note: i will be sys.argv[3].

--- callsubprocess.py
+++ callsubprocess.py
@@ -1,9 +1,10 @@
 import subprocess
+import sys
 
 f = open("GAN.txt", "r")
 index = f.readline()
 if index == "0":
-    command = ["runGan.py", "1"]
+    command = ["runGan.py", "1", sys.argv[1], sys.argv[2], sys.argv[3]]
 elif index == "1":
     command = "mosaicesrgan.py"
 subprocess.call(command, shell=True)
