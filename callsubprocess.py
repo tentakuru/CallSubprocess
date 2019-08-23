35e54ebabb7bab5f7ffffb123c5532169acdc825
tentakuru <53518872+tentakuru@users.noreply.github.com>
2019-08-23 12:19:53 +0200

    Add options

--- callsubprocess.py
+++ callsubprocess.py
@@ -6,5 +6,9 @@ index = f.readline()
 if index == "0":
     command = ["runGan.py", "1", sys.argv[1], sys.argv[2], sys.argv[3]]
 elif index == "1":
-    command = "mosaicesrgan.py"
+    command = ["runGan.py", "1", sys.argv[1], sys.argv[2], sys.argv[3], "tentakuru"]
+elif index == "2":
+    command = ["mosaicesrgan.py"]
+elif index == "3":
+    command = ["runGan.py", "1", sys.argv[1], sys.argv[2], "1", sys.argv[3]]
 subprocess.call(command, shell=True)
