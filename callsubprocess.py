1328c4449287a1980dd04f329de188f2f17a1d6b
tentakuru <53518872+tentakuru@users.noreply.github.com>
2019-08-26 13:20:11 +0200

    Add files via upload

--- callsubprocess.py
+++ callsubprocess.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import os
 
 f = open("GAN.txt", "r")
 index = f.readline()
@@ -10,5 +11,9 @@ elif index == "1":
 elif index == "2":
     command = ["mosaicesrgan.py"]
 elif index == "3":
-    command = ["runGan.py", "1", sys.argv[1], sys.argv[2], "1", sys.argv[3]]
+    command = ["runGan.py", "1", sys.argv[1], sys.argv[2], "1", sys.argv[3]]  
+elif index == "4":
+    command = ["runGan104.py", "1", sys.argv[1], sys.argv[2], "0"]    
+elif index == "104":
+    command = [os.path.dirname(os.getcwd()) + "\\runGan104.py", "1", sys.argv[1], sys.argv[2], "0"]
 subprocess.call(command, shell=True)
