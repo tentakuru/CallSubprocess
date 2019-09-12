0fe779e2326702fc23bd27f9d97529791cc5a3b8
tentakuru <53518872+tentakuru@users.noreply.github.com>
2019-09-12 18:47:56 +0200

    Add files via upload

--- callsubprocess.py
+++ callsubprocess.py
@@ -13,7 +13,13 @@ elif index == "2":
 elif index == "3":
     command = ["runGan.py", "1", sys.argv[1], sys.argv[2], "1", sys.argv[3]]  
 elif index == "4":
-    command = ["runGan104.py", "1", sys.argv[1], sys.argv[2], "0"]    
+    command = ["runGan104.py", "1", sys.argv[1], sys.argv[2], "0"]
+elif index == "5":
+    command = ["runGan104.py", "1", sys.argv[1], sys.argv[2], "1"]  
+elif index == "6":
+    command = ["mosaicesrgan104.py"]   
 elif index == "104":
     command = [os.path.dirname(os.getcwd()) + "\\runGan104.py", "1", sys.argv[1], sys.argv[2], "0"]
+elif index == "102":
+    command = ["runGan102.py", "1"]
 subprocess.call(command, shell=True)
